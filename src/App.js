2a69b2e468538c349eab3897a5f7f81fce9c4ad9
DESKTOP-3TIE0CK\Ruslan <komesarus777@gmail.com>
2022-06-20 12:28:17 +0300

    Added custom select.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './style/App.scss';
 import RUserList from './components/UI/list/RUserList';
 import UserForm from './components/UserForm';
+import RSelect from './components/UI/select/RSelect';
+import RInput from './components/UI/input/RInput';
 import { useState } from 'react';
 
 function App() {
 
 
-  const [users, setUsers] = useState([{ id: 1, name: 'Roman', surname: 'Bobchik' }, { id: 2, name: 'Andry', surname: 'Black' }])
+  const [users, setUsers] = useState([{ id: 1, name: 'Roman', surname: 'Bobchik' }, { id: 2, name: 'Andry', surname: 'Black' }, { id: 3, name: 'fffff', surname: 'AsAs' }])
+  const [selectedSort, setSelectedSort] = useState('')
+
   const createPost = (newUser) => {
     setUsers([...users, newUser])
   }
@@ -15,13 +19,27 @@ function App() {
     setUsers(users.filter(u => u.id !== user.id))
   }
 
+  const sortUsers = (sort) => {
+    setSelectedSort(sort)
+    setUsers([...users].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <div className='container'>
         <UserForm create={createPost}></UserForm>
       </div>
+      <hr />
       <div className='container'>
-        <RUserList remove={removeUser} users={users}></RUserList>
+        <RInput />
+        <RSelect defaultValue='Sort by'
+          value={selectedSort}
+          onChange={sortUsers}
+          options={[{ value: 'name', name: 'By name' }, { value: 'surname', name: 'By surname' }]}>
+        </RSelect>
+      </div>
+      <div className='container'>
+        {users.length ? <RUserList remove={removeUser} users={users}></RUserList> : <h1>We don't have users</h1>}
       </div>
     </div>
   );
---
+++ src/components/UI/select/RSelect.jsx
@@ -0,0 +1,17 @@
+
+
+const RSelect = ({ options, defaultValue, value, onChange }) => {
+    return (
+        <select value={value} onChange={e => onChange(e.target.value)}>
+            <option value="">{defaultValue}</option>
+            {options.map(option =>
+                <option key={option.value} value={option.value}>
+                    {option.name}
+                </option>
+            )}
+        </select>
+    )
+}
+
+
+export default RSelect;
