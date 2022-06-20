4122f4280c7eacaa3e19bffe2b4c57803f7d62c2
DESKTOP-3TIE0CK\Ruslan <komesarus777@gmail.com>
2022-06-20 14:03:07 +0300

    Added search.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import RUserList from './components/UI/list/RUserList';
 import UserForm from './components/UserForm';
 import RSelect from './components/UI/select/RSelect';
 import RInput from './components/UI/input/RInput';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 function App() {
 
 
   const [users, setUsers] = useState([{ id: 1, name: 'Roman', surname: 'Bobchik' }, { id: 2, name: 'Andry', surname: 'Black' }, { id: 3, name: 'fffff', surname: 'AsAs' }])
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
 
-  const createPost = (newUser) => {
+  const createUser = (newUser) => {
     setUsers([...users, newUser])
   }
 
@@ -19,19 +20,32 @@ function App() {
     setUsers(users.filter(u => u.id !== user.id))
   }
 
+  const sortedUsers = useMemo(() => {
+    if (selectedSort) {
+      return [...users].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return users;
+  }, [selectedSort, users])
+
+
+  const sortedAndSearchedUsers = useMemo(() => {
+    return sortedUsers.filter(user => user.name.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedUsers])
+
+
   const sortUsers = (sort) => {
     setSelectedSort(sort)
-    setUsers([...users].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div className="App">
       <div className='container'>
-        <UserForm create={createPost}></UserForm>
+        <UserForm create={createUser}></UserForm>
       </div>
       <hr />
       <div className='container'>
-        <RInput />
+        <RInput value={searchQuery} placeholder='search' onChange={e => setSearchQuery(e.target.value)} />
+        {searchQuery}
         <RSelect defaultValue='Sort by'
           value={selectedSort}
           onChange={sortUsers}
@@ -39,7 +53,7 @@ function App() {
         </RSelect>
       </div>
       <div className='container'>
-        {users.length ? <RUserList remove={removeUser} users={users}></RUserList> : <h1>We don't have users</h1>}
+        {sortedAndSearchedUsers.length ? <RUserList remove={removeUser} users={sortedAndSearchedUsers}></RUserList> : <h1>We don't have users</h1>}
       </div>
     </div>
   );
